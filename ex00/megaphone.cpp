f6058ac54f58af6569bcee9b9e545e8896250557
hikaru <ichi-hikaru@i.softbank.jp>
2026-04-03 14:06:16 +0900

    complete inplementing meegaphone

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -1,3 +1,14 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ichikawahikaru <ichikawahikaru@student.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/30 19:57:22 by ichikawahik       #+#    #+#             */
+/*   Updated: 2026/02/12 18:54:18 by ichikawahik      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include <cstring>
 #include <iostream>
@@ -10,16 +21,15 @@ int main(int argc, char **argv)
 	j = 0;
 	if (argc == 1)
 	{
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std:: end1;
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 		return 0;
 	}
 	while (argv[++j])
 	{
 		i = -1;
-		while(argv[j][++j])
+		while(argv[j][++i])
 			std::cout << (char)toupper(argv[j][i]);
 	}
-	std::cout << std::end1;
+	std::cout << std::endl;
 	return 0;
 }
-
