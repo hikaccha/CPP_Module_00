db2b4ad5de213f457bb840038cd0e9c29298c99f
hikaru <ichi-hikaru@i.softbank.jp>
2026-04-03 15:51:07 +0900

    refactoring megaphone.cpp file.

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   megaphone.cpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ichikawahikaru <ichikawahikaru@student.    +#+  +:+       +#+        */
+/*   By: hichikaw <hichikaw@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/30 19:57:22 by ichikawahik       #+#    #+#             */
-/*   Updated: 2026/02/12 18:54:18 by ichikawahik      ###   ########.fr       */
+/*   Updated: 2026/04/03 15:44:15 by hichikaw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,18 +18,18 @@ int main(int argc, char **argv)
 	int	i;
 	int	j;
 
-	j = 0;
-	if (argc == 1)
+	j = -1;
+	if (argc < 2)
 	{
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
-		return 0;
+		return (0);
 	}
-	while (argv[++j])
+	while (argc > 1 && argv[++j])
 	{
 		i = -1;
-		while(argv[j][++i])
-			std::cout << (char)toupper(argv[j][i]);
+		while(j > 0 && argv[j][++i])
+			std::cout << (char)std::toupper(argv[j][i]);
 	}
 	std::cout << std::endl;
-	return 0;
+	return (0);
 }
